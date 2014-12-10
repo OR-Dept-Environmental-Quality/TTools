5ba0e29f75fd250f448d216eb953f51f6b639686
rmichie <michie.ryan@deq.state.or.us>
2014-12-09 18:37:51 -0800

    updated to v 0.92
    - updated so morph and landcover outputs are created
    - updated to reflect PEP 8 (more to come)
    - fixed bugs

--- Output_To_csv.py
+++ Output_To_csv.py
@@ -1,19 +1,20 @@
 #######################################################################################
 # TTools
-# Output_To_csv - v 0.91
+# Output_To_csv - v 0.92
 # Ryan Michie
 
-# Output_To_csv will take the point feature created from using steps 1-5 and output a landcover data csv file formatted 
-# for heat source 9.
+# Output_To_csv will take the node point feature created from using steps 1-5 and 
+# output landcover and morphology data csv files formatted for heat source 9.
 
 # INPUTS
-# 0: Input TTools point feature class (NodesFC)
-# 1: create seperate csv files for each STREAM_ID (multiplecsv) True/False. if True will appends stream ID to csv name.
-# 2: path directory where output csv file will be saved (outcsv_dir)
-# 3: name of the csv file (outcsv_file)
+# 0: Input TTools point feature class (nodes_fc)
+# 1: create seperate csv files for each STREAM_ID (multiplecsv) True/False. if True will append stream ID to csv name.
+# 2: path directory where the output csv file will be saved (outputdir)
+# 3: name of the landcover data csv file (lcdatafile)
+# 4: name of the morphology data csv file (morphfile)
 
 # OUTPUTS
-# csv file, with the same columns and order as the input point file (minus a few feature class specfic columns)
+# landcover and morphology data csv files formatted for input into heatsource 9
 
 # This version is for manual starts from within python.
 # This script requires Python 2.6 and ArcGIS 10.1 or higher to run.
@@ -22,59 +23,71 @@
 
 # Import system modules
 from __future__ import division, print_function
-import sys, os, string, gc, shutil, time
+import sys
+import os
+import gc
+import time
 import arcpy
+from os.path import join
+from datetime import timedelta
+from math import ceil
 from collections import defaultdict
 from operator import itemgetter
 import csv
 
 # Parameter fields for python toolbox
-#NodesFC = parameters[0].valueAsText
+#nodes_fc = parameters[0].valueAsText
 #multiplecsv = parameters[1].valueAsText
-#outcsv_dir = parameters[2].valueAsText
-#outcsv_file = parameters[3].valueAsText
+#outputdir = parameters[2].valueAsText
+#lcdatafile = parameters[3].valueAsText
+#morphfile = parameters[4].valueAsText
 
 # Start Fill in Data
-NodesFC = r"D:\Projects\TTools_9\Example_data.gdb\out_nodes"
-multiplecsv = "True"
-outcsv_dir = r"D:\Projects\TTools_9"
-outcsv_file = "out_nodes.csv"
+nodes_fc = r"D:\Projects\TTools_9\JohnsonCreek.gdb\jc_stream_nodes"
+multiplecsv = False
+outputdir = r"D:\Projects\TTools_9"
+lcdatafile = "lcdata.csv"
+morphfile = "morphdata.csv"
 # End Fill in Data
 
-def ReadNodesFC(NodesFC, readfields):
+def read_nodes_fc(nodes_fc, readfields):
     """Reads an input point file and returns the NODE ID and X/Y coordinates as a nested dictionary"""
-    pnt_dict = NestedDictTree()
-    Incursorfields = ["STREAM_ID","NODE_ID"] + readfields
+    nodeDict = nested_dict()
+    incursorFields = ["STREAM_ID","NODE_ID"] + readfields
     # Determine input point spatial units
-    proj = arcpy.Describe(NodesFC).spatialReference
-    with arcpy.da.SearchCursor(NodesFC,Incursorfields,"",proj) as Inrows:
+    proj = arcpy.Describe(nodes_fc).spatialReference
+    with arcpy.da.SearchCursor(nodes_fc,incursorFields,"",proj) as Inrows:
         for row in Inrows:
             for f in xrange(0,len(readfields)):
-                pnt_dict[row[0]][row[1]][readfields[f]] = row[2+f]
-    return(pnt_dict)
+                nodeDict[row[0]][row[1]][readfields[f]] = row[2+f]
+    return(nodeDict)
 
-def write_csv(csvlist, csvfile):
-    """write the input list to csv"""
-    with open(outcsv_final, "wb") as f:
-        writer = csv.writer(f)
-        writer.writerows(csv_out)    
+def write_csv(outputdir, filename, colnames, outlist):
+    """write the output list to csv"""
+    
+    # insert column header names
+    outlist.insert(0, colnames)
+    
+    with open(join(outputdir, filename), "wb") as file_object:
+        writer = csv.writer(file_object,  dialect= "excel")
+        writer.writerows(outlist)        
 
-def NestedDictTree(): 
+def nested_dict(): 
     """Build a nested dictionary"""
-    return defaultdict(NestedDictTree)
+    return defaultdict(nested_dict)
 
 def read_csv(csvfile):
     """Reads an input csv file and returns the header row as a list and the data as a nested dictionary"""
-    csv_dict = NestedDictTree()
+    csv_dict = nested_dict()
     with open(csvfile, "rb") as f:
         reader = csv.reader(f)
         header = reader.next()
-        if header[0] != "NODE_ID":
-            sys.exit("csv file does not have NODE_ID as first column")
+        if header[0] != "STREAM_ID":
+            sys.exit("csv file does not have STREAM_ID as first column")
         for row in reader:
             for key in xrange(0,len(header)):
                 csv_dict[row[0]][header[key]] = row[key]
-    return(header,csv_dict)
+    return(header, csv_dict)
 
 #enable garbage collection
 gc.enable()
@@ -84,57 +97,92 @@ try:
     startTime= time.time()
     arcpy.AddMessage("Export to csv") 
 
-    removelist = [u"OBJECTID",u"Id",u"Shape",u"ELEVATION",u"GRADIENT",u"NUM_DIR",u"NUM_ZONES",u"SAMPLE_DIS"]
+    # Get all the column headers in the point file
+    nodes_fc_headers = [field.name for field in arcpy.Describe(nodes_fc).fields]
+    
+    id_headers = ["STREAM_ID", "NODE_ID"]    
+    lcdataheaders =["STREAM_KM","LONGITUDE","LATITUDE","TOPO_W","TOPO_S","TOPO_E"]
+    morphheaders = ["STREAM_KM","ELEVATION","GRADIENT","BOTTOM_WIDTH",
+                    "CHANNEL_ANGLE_Z","MANNINGS_n","SED_THERMAL_CONDUCTIVITY",
+                    "SED_THERMAL_DIFFUSIVITY","SED_HYPORHEIC_THICKNESSS",
+                    "%HYPORHEIC_EXCHANGE","POROSITY"]
+    
+    lcsample_headers = ["LC_T","ELE_T","LAI_T","k_T","CAN_T","OH_T"]
+    
+    # This gets the landcover sample colnames by looking for them iteratively    
+    for header in nodes_fc_headers:
+        if any(txt in header for txt in lcsample_headers):
+            lcdataheaders.append(header)    
+    file_header_list = [lcdataheaders, morphheaders]
+    file_name_list = [lcdatafile, morphfile]
 
-    # Get all the column headers in the point file and remove the ones in removelist
-    header = [field.name for field in arcpy.Describe(NodesFC).fields]
-    header_clean = [h for h in header if h not in removelist]
+    nodeDict = read_nodes_fc(nodes_fc, nodes_fc_headers)
 
-    NODES = read_pointfile(NodesFC, header_clean)
-
-    ####################################################################################################### 
-    # Output the csv file
-
-    # Get the stream km dictionary keys and sort them
-    #NODE_keys = NODES.keys()
-    #NODE_keys.sort()    
-
+    # Output the csv file 
     # make a wide format list by node ID from the nested dictionary
-
-    if multiplecsv == "True":
-        for streamID in NODES:
-            csv_out = [[NODES[streamID][nodeID][h] for h in header_clean] for nodeID in NODES[streamID]]
-            outcsv_final = outcsv_dir + "\\" + outcsv_file.replace(".csv", "") + "_" + str(streamID) + ".csv"
-
-            #sort the list by stream km
-            csv_out = sorted(csv_out, key=itemgetter(1), reverse=True)
-
-            # Add the header row
-            csv_out.insert(0,header_clean)	    
-
-            # write it
-            write_csv(csv_out,outcsv_final)
+    n_nodes = 0
+    if multiplecsv is True:
+        for streamID in nodeDict:
+            i = 0
+            for ouput_header_list in file_header_list:
+                outlist = []
+                colnames = id_headers + ouput_header_list
+                n_nodes = n_nodes + len(nodeDict[streamID])
+                for nodeID in nodeDict[streamID]:
+                    row_list = []
+                    for header in colnames:
+                        if header in nodeDict[streamID][nodeID].keys():
+                            val = nodeDict[streamID][nodeID][header]
+                            row_list.append(val)
+                        else:
+                            # use None when there is no data in the Nodes feature class
+                            row_list.append(None)
+                    outlist.append(row_list)
+                       
+                #sort the list by stream km
+                outlist = sorted(outlist, key=itemgetter(1), reverse=True)
+            
+                # name the output
+                filename = file_name_list[i].replace(".csv", "") + "_" + str(streamID) + ".csv"
+    
+                # write it
+                write_csv(outputdir, filename, colnames, outlist)
+                i = i + 1
 
     else:
-        csv_out = [[NODES[streamID][nodeID][h] for h in header_clean] for streamID in NODES for nodeID in NODES[streamID]]
-        outcsv_final = outcsv_dir+ "\\" + outcsv_file
+        i = 0
+        for ouput_header_list in file_header_list:
+            outlist = []
+            for streamID in nodeDict:
+                colnames = id_headers + ouput_header_list
+                n_nodes = n_nodes + len(nodeDict[streamID])
+                for nodeID in nodeDict[streamID]:
+                    row_list = []
+                    for header in colnames:
+                        if header in nodeDict[streamID][nodeID].keys():
+                            val = nodeDict[streamID][nodeID][header]
+                            row_list.append(val)
+                        else:
+                            # use None when there is no data in the Nodes feature class
+                            row_list.append(None)
+                    outlist.append(row_list)
 
-
-        #sort the list by stream ID and then stream km
-        csv_out = sorted(csv_out, key=itemgetter(1,2), reverse=True)	
-
-        # Add the header row
-        csv_out.insert(0,header_clean)
-
-        # write it
-        write_csv(csv_out,outcsv_final)
-
-    gc.collect()
+            #sort the list by stream ID and then stream km
+            outlist = sorted(outlist, key=itemgetter(1,2), reverse=True)	
+    
+            # name the output
+            filename = file_name_list[i]
+    
+            # write it
+            write_csv(outputdir, filename, colnames, outlist)
+            i = i + 1
 
     endTime = time.time()
-    elapsedmin= (endTime - startTime) / 60	
-    print("Process Complete in %s minutes" % (elapsedmin))
-    arcpy.AddMessage("Process Complete at %s, %s minutes" % (endTime, elapsedmin))    
+    gc.collect()
+    
+    elapsedmin= ceil(((endTime - startTime) / 60)* 10)/10
+    mspernode = timedelta(seconds=(endTime - startTime) / n_nodes / 2).microseconds
+    print("Process Complete in %s minutes. %s microseconds per node" % (elapsedmin, mspernode))    
 
 # For arctool errors
 except arcpy.ExecuteError:
