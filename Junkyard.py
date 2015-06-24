13346a59e763923d33c4137d17f5167ee3e7a5ca
rmichie <michie.ryan@deq.state.or.us>
2015-06-24 09:54:14 -0700

    Added different node fc update methods

--- Junkyard.py
+++ Junkyard.py
@@ -1,5 +1,28 @@
 # This is a junkyard for old methods or code that I want to keep around just in case.
 
+def update_nodes_fc1(NodeDict, streamID, nodeID, NodesFC, AddFields):
+    """Updates a single node in the input point feature class with data
+    from the nodes dictionary"""
+
+    # Build a query to retreive just the node row
+    whereclause = """%s = %s""" % (arcpy.AddFieldDelimiters(NodesFC, "NODE_ID"), nodeID)
+
+    with arcpy.da.UpdateCursor(NodesFC,["NODE_ID"] + AddFields, whereclause) as cursor:
+        for row in cursor:
+            for f in xrange(0,len(AddFields)):
+                row[f+1] = NodeDict[streamID][nodeID][AddFields[f]]
+                cursor.updateRow(row)
+
+def update_nodes_fc2(UpdateValue, nodeID, NodesFC, UpdateField):
+    """Updates the input point feature class with the new value"""
+    
+    whereclause = """%s = %s""" % (arcpy.AddFieldDelimiters(NodesFC, "NODE_ID"), nodeID)
+
+    with arcpy.da.UpdateCursor(NodesFC,["NODE_ID"] + UpdateField, whereclause) as cursor:
+        for row in cursor:   
+            row[1] = UpdateValue
+            cursor.updateRow(row)
+
 def read_csv(csvfile):
     """Reads an input csv file and returns the header row as a list and the data as a nested dictionary"""
     csv_dict = nested_dict()
@@ -16,35 +39,35 @@ def read_csv(csvfile):
 def CalcChannelWidthAdvancedLic(nodexy, rb, lb):
     rb_near_result = arcpy.analysis.GenerateNearTable(
         nodexy, rb, r'in_memory\neartable', '', 'LOCATION','NO_ANGLE', 'CLOSEST')
-    
+
     with arcpy.da.SearchCursor(rb_near_result, ['NEAR_DIST']) as rows:
-            row = rows.next()
-            rb_distance = row[0]    
-    
+        row = rows.next()
+        rb_distance = row[0]    
+
     lb_near_result = arcpy.analysis.GenerateNearTable(
         nodexy, lb, r'in_memory\neartable', '', 'LOCATION','NO_ANGLE', 'CLOSEST')
-    
+
     with arcpy.da.SearchCursor(lb_near_result, ['NEAR_DIST']) as rows:
-                row = rows.next()
-                lb_distance = row[0]    
+        row = rows.next()
+        lb_distance = row[0]    
     return(rb_distance + lb_distance)
 
 def GetTopoAngles2(NodeDict, streamID, EleRaster, azimuths, azimuthdisdict, con_z_to_m):
     """This gets the maximum shade angle for each stream and outputs the data as a list. 
     This version generates an array for each stream. It is faster but may crash if the arrays are large"""
-    
+
     TopoList = []
     CoordList = []
-    
+
     cellsizeX = arcpy.Describe(EleRaster).meanCellWidth
     cellsizeY = arcpy.Describe(EleRaster).meanCellHeight
-    
+
     # calculate the buffer distance (in EleRaster spatial units) to add to the raster bounding box when extracting to an array
     buffer = cellsizeX * 2    
-    
+
     Nodes = NodeDict.keys()
     Nodes.sort()
-    
+
     for nodeID in Nodes:
         origin_x = NodeDict[nodeID]["POINT_X"]
         origin_y = NodeDict[nodeID]["POINT_Y"]      
@@ -56,11 +79,11 @@ def GetTopoAngles2(NodeDict, streamID, EleRaster, azimuths, azimuthdisdict, con_
 
                 # Add the all the coordinates to the list
                 CoordList.append([topoX, topoY, nodeID, a])
-            
+
     # calculate lower left corner coordinate and nrows/cols for the bounding box
     # first transpose the list so x and y coordinates are in the same vector
     tlist = map(lambda *i: list(i), *CoordList)
-      
+
     Xmin = min(tlist[0]) - buffer
     Ymin = min(tlist[1]) - buffer
     Ymax = max(tlist[1]) + buffer            
@@ -69,28 +92,28 @@ def GetTopoAngles2(NodeDict, streamID, EleRaster, azimuths, azimuthdisdict, con_
     bbox_lower_left = arcpy.Point(Xmin, Ymin) # must be in raster map units
     bbox_upper_left = [Xmin, Ymax, cellsizeX, cellsizeY]
     nodata_to_value = -9999 / con_z_to_m
-    
+
     del(tlist)
-    
+
     # Construct the array. Note returned array is (row, col) so (y, x)
     Zarry = arcpy.RasterToNumPyArray(EleRaster, bbox_lower_left, ncols, nrows, nodata_to_value)
-    
+
     # convert array values to meters if needed
     Zarry = Zarry * con_z_to_m             
-    
+
     # iterate through each node and azimuth
     for nodeID in NodeDict:
         for a in azimuths:
             CoordList2 = [row for row in CoordList if row[2] == nodeID and row[3] == a]
             TopoZList = []
             #xy = []
-    
+
             for i in range(0,len(CoordList2)):
                 xy = CoordToArray(CoordList2[i][0], CoordList2[i][1], bbox_upper_left)
                 TopoZList.append(Zarry[xy[1], xy[0]])
                 #xy.append(CoordToArray(CoordList2[i][0], CoordList2[i][1], bbox_upper_left))
                 #TopoZList.append(1)
-            
+
             TopoZarry = np.array(TopoZList)
             Disarry = azimuthdisdict[a] * con_to_m
             Shadearry = np.degrees(np.arctan((TopoZarry - TopoZarry[0]) / Disarry))
@@ -108,14 +131,14 @@ def GetTopoAngles2(NodeDict, streamID, EleRaster, azimuths, azimuthdisdict, con_
             ShadeAngle_X = (ShadeDistance * con_from_m * sin(radians(a))) + CoordList2[0][0]
             ShadeAngle_Y = (ShadeDistance * con_from_m * cos(radians(a))) + CoordList2[0][1]
             offRasterSamples = (TopoZarry > -9998).sum()
-        
+
             TopoList.append([ShadeAngle_X, ShadeAngle_Y, ShadeAngle_X, ShadeAngle_Y, streamID, nodeID, a, ShadeAngle, ShadeZ, TopoZarry[0], ZChange, ShadeDistance, SearchDistance, offRasterSamples])
 
     return TopoList
 
 def Downloadcomtypes():
     import urllib
-    
+
     print("Downloading comtypes")
     url = "https://pypi.python.org/packages/source/c/comtypes/comtypes-1.1.1.zip"
     file_name = url.split('/')[-1]
@@ -139,13 +162,13 @@ def GetLibPath():
     if not os.path.isfile(esriComDir+'\\esriSystemUI.olb'):
         keyESRI = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\\ESRI\\Desktop10.1")
         esriComDir = _winreg.QueryValueEx(keyESRI,"InstallDir") [0] + "com\\"
-    
+
     if not os.path.isfile(esriComDir+'\\esriSystemUI.olb'):
-            keyESRI = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\\ESRI\\Desktop10.2")
-            esriComDir = _winreg.QueryValueEx(keyESRI,"InstallDir") [0] + "com\\"    
+        keyESRI = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\\ESRI\\Desktop10.2")
+        esriComDir = _winreg.QueryValueEx(keyESRI,"InstallDir") [0] + "com\\"    
 
     return esriComDir
-    
+
 def GetModule(sModuleName):
     """Import ArcGIS module"""
     from comtypes.client import GetModule
@@ -170,7 +193,7 @@ def DistanceBetweenPoints(Xa,Ya,Xb,Yb):
     return dist
 
 def CalcAspect(dx, dy):
-    
+
     RadAngle = atan2(dY,dX)
     aspect = (RadAngle * 180) / pi
     aspect2 = degrees(atan2(dY,dX))
